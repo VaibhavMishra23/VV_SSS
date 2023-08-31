383e914c9ef192cc09adbc6ff1ea60cb96462ba4
VaibhavMishra23 <130959238+VaibhavMishra23@users.noreply.github.com>
2023-08-31 15:57:47 +0530

    Add files via upload

---
+++ frontend.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var element = document.getElementById("show1");
+    var element2 = document.getElementById("show2");
+    var element3 = document.getElementById("navbar");
+    element.classList.toggle("he");
+    element.classList.toggle("son");
+    element2.classList.toggle("son");
+ }
